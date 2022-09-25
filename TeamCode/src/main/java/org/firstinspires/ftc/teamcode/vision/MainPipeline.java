da1c45714b8b15fc1b42bcbca3d8dedbc66b0bf1
Rikhil2 <rikhiljkumar@gmail.com>
2022-09-25 17:46:37 -0400

    mask for pipes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/MainPipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/MainPipeline.java
@@ -18,49 +18,9 @@ public class MainPipeline extends OpenCvPipeline {
     @Override
     public Mat processFrame(Mat input) {
         Imgproc.cvtColor(input, ycrcbMat, Imgproc.COLOR_RGB2YCrCb);
-
-        /*
-         * This is where our thresholding actually happens.
-         * Takes our "ycrcbMat" as input and outputs a "binary"
-         * Mat to "binaryMat" of the same size as our input.
-         * "Discards" all the pixels outside the bounds specified
-         * by the scalars above (and modifiable with EOCV-Sim's
-         * live variable tuner.)
-         *
-         * Binary meaning that we have either a 0 or 255 value
-         * for every pixel.
-         *
-         * 0 represents our pixels that were outside the bounds
-         * 255 represents our pixels that are inside the bounds
-         */
         Core.inRange(ycrcbMat, lower, upper, binaryMat);
-
-        /*
-         * Release the reusable Mat so that old data doesn't
-         * affect the next step in the current processing
-         */
         maskedInputMat.release();
-
-        /*
-         * Now, with our binary Mat, we perform a "bitwise and"
-         * to our input image, meaning that we will perform a mask
-         * which will include the pixels from our input Mat which
-         * are "255" in our binary Mat (meaning that they're inside
-         * the range) and will discard any other pixel outside the
-         * range (RGB 0, 0, 0. All discarded pixels will be black)
-         */
         Core.bitwise_and(input, input, maskedInputMat, binaryMat);
-
-        /*
-         * The Mat returned from this method is the
-         * one displayed on the viewport.
-         *
-         * To visualize our threshold, we'll return
-         * the "masked input mat" which shows the
-         * pixel from the input Mat that were inside
-         * the threshold range.
-         */
-
         Scalar GREEN = new Scalar(0, 255, 0);
         Scalar BLUE = new Scalar(0, 0, 255);
         Scalar RED = new Scalar(255, 0, 0);
