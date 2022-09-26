b6e9e4ffdce178dbb118ef8ae1ad2f45236bfa85
Rikhil2 <rikhiljkumar@gmail.com>
2022-09-26 11:21:58 -0400

    fix test pipeline

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/visionTesting/TestPipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/visionTesting/TestPipeline.java
@@ -6,6 +6,7 @@ import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
+import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvPipeline;
 
@@ -18,8 +19,11 @@ public class TestPipeline extends OpenCvPipeline {
     Telemetry telemetry;
     List<MatOfPoint> yellowContours = new ArrayList<MatOfPoint>();
 
+    double k = 1;
+
     Rect YellowRect = new Rect();
-    List <Rect> yellowRect = new ArrayList<Rect>();
+    List <MatOfPoint> yellowRect = new ArrayList<MatOfPoint>();
+    List <Double> distance = new ArrayList<Double>();
 
     public TestPipeline(Telemetry telemetry) {
         this.telemetry = telemetry;
@@ -34,27 +38,49 @@ public class TestPipeline extends OpenCvPipeline {
     private Mat ycrcbMat       = new Mat();
     private Mat binaryMat      = new Mat();
     private Mat maskedInputMat = new Mat();
+    Mat morphedThreshold = new Mat();
+
+    Mat hierarchy = new Mat();
+
+    Scalar RED = new Scalar(255, 0, 0);
+    Scalar GREEN = new Scalar(0, 255, 0);
+
+    Mat erodeElement = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(15, 15));
+    Mat dilateElement = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(15, 15));
+
+    void morphMask(Mat input, Mat output)
+    {
+        /*
+         * Apply some erosion and dilation for noise reduction
+         */
+
+        Imgproc.erode(input, output, erodeElement);
+        Imgproc.erode(output, output, erodeElement);
+
+        Imgproc.dilate(output, output, dilateElement);
+        Imgproc.dilate(output, output, dilateElement);
+    }
+
 
     @Override
     public Mat processFrame(Mat input) {
         Imgproc.cvtColor(input, ycrcbMat, Imgproc.COLOR_RGB2YCrCb);
-        Core.inRange(ycrcbMat, lower, upper, binaryMat);
-        maskedInputMat.release();
-        Core.bitwise_and(input, input, maskedInputMat, binaryMat);
-        Scalar RED = new Scalar(255, 0, 0);
+        Core.extractChannel(ycrcbMat, ycrcbMat, 2);
+        Imgproc.threshold(ycrcbMat, binaryMat, 80, 110, Imgproc.THRESH_BINARY_INV);
+        morphMask(binaryMat, morphedThreshold);
 
-        Imgproc.findContours(maskedInputMat, yellowContours, new Mat(), Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
+        yellowContours.clear();
+        yellowRect.clear();
+
+        Imgproc.findContours(morphedThreshold, yellowContours, new Mat(), Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
         Imgproc.drawContours(input, yellowContours, -1, RED);
 
-        for (int i = 0; i < yellowContours.size(); i++) {
-            if (Imgproc.contourArea(yellowContours.get(i)) < minSize) {
-                biggestContour = Collections.max(yellowContours, (t0, t1) -> {
-                    return Double.compare(Imgproc.boundingRect(t0).width, Imgproc.boundingRect(t1).width);
-                });
-                YellowRect = Imgproc.boundingRect(biggestContour);
-                yellowRect.add(Imgproc.boundingRect(yellowContours.get(i)));
+        for (int i = 0; i < yellowContours.size(); i++){
+            for (int j = 0; j < 3; j++) {
+                distance.add((double)yellowContours.get(i).height());
             }
         }
-        return maskedInputMat;
+
+        return input;
     }
 }
