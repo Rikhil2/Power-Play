6df2ab081167bba5f615e671ea0d5ee536d47970
Rikhil2 <rikhiljkumar@gmail.com>
2022-10-04 22:41:27 -0400

    apriltag

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/visionTesting/TestPipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/visionTesting/TestPipeline.java
@@ -1,9 +1,16 @@
 package org.firstinspires.ftc.teamcode.visionTesting;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.opencv.calib3d.Calib3d;
 import org.opencv.core.Core;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfDouble;
 import org.opencv.core.MatOfPoint;
+import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.MatOfPoint3f;
+import org.opencv.core.Point;
+import org.opencv.core.Point3;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
@@ -73,6 +80,97 @@ public class TestPipeline extends OpenCvPipeline {
             detectionsUpdate = null;
             return ret;
         }
+    }
+
+    void constructMatrix()
+    {
+        //     Construct the camera matrix.
+        //
+        //      --         --
+        //     | fx   0   cx |
+        //     | 0    fy  cy |
+        //     | 0    0   1  |
+        //      --         --
+        //
+
+        cameraMatrix = new Mat(3,3, CvType.CV_32FC1);
+
+        cameraMatrix.put(0,0, fx);
+        cameraMatrix.put(0,1,0);
+        cameraMatrix.put(0,2, cx);
+
+        cameraMatrix.put(1,0,0);
+        cameraMatrix.put(1,1,fy);
+        cameraMatrix.put(1,2,cy);
+
+        cameraMatrix.put(2, 0, 0);
+        cameraMatrix.put(2,1,0);
+        cameraMatrix.put(2,2,1);
+    }
+
+    void drawAxisMarker(Mat buf, double length, int thickness, Mat rvec, Mat tvec, Mat cameraMatrix)
+    {
+        // The points in 3D space we wish to project onto the 2D image plane.
+        // The origin of the coordinate space is assumed to be in the center of the detection.
+        MatOfPoint3f axis = new MatOfPoint3f(
+                new Point3(0,0,0),
+                new Point3(length,0,0),
+                new Point3(0,length,0),
+                new Point3(0,0,-length)
+        );
+
+        // Project those points
+        MatOfPoint2f matProjectedPoints = new MatOfPoint2f();
+        Calib3d.projectPoints(axis, rvec, tvec, cameraMatrix, new MatOfDouble(), matProjectedPoints);
+        Point[] projectedPoints = matProjectedPoints.toArray();
+
+        // Draw the marker!
+        Imgproc.line(buf, projectedPoints[0], projectedPoints[1], red, thickness);
+        Imgproc.line(buf, projectedPoints[0], projectedPoints[2], green, thickness);
+        Imgproc.line(buf, projectedPoints[0], projectedPoints[3], blue, thickness);
+
+        Imgproc.circle(buf, projectedPoints[0], thickness, white, -1);
+    }
+
+    void draw3dCubeMarker(Mat buf, double length, double tagWidth, double tagHeight, int thickness, Mat rvec, Mat tvec, Mat cameraMatrix)
+    {
+        //axis = np.float32([[0,0,0], [0,3,0], [3,3,0], [3,0,0],
+        //       [0,0,-3],[0,3,-3],[3,3,-3],[3,0,-3] ])
+
+        // The points in 3D space we wish to project onto the 2D image plane.
+        // The origin of the coordinate space is assumed to be in the center of the detection.
+        MatOfPoint3f axis = new MatOfPoint3f(
+                new Point3(-tagWidth/2, tagHeight/2,0),
+                new Point3( tagWidth/2, tagHeight/2,0),
+                new Point3( tagWidth/2,-tagHeight/2,0),
+                new Point3(-tagWidth/2,-tagHeight/2,0),
+                new Point3(-tagWidth/2, tagHeight/2,-length),
+                new Point3( tagWidth/2, tagHeight/2,-length),
+                new Point3( tagWidth/2,-tagHeight/2,-length),
+                new Point3(-tagWidth/2,-tagHeight/2,-length));
+
+        // Project those points
+        MatOfPoint2f matProjectedPoints = new MatOfPoint2f();
+        Calib3d.projectPoints(axis, rvec, tvec, cameraMatrix, new MatOfDouble(), matProjectedPoints);
+        Point[] projectedPoints = matProjectedPoints.toArray();
+
+        // Pillars
+        for(int i = 0; i < 4; i++)
+        {
+            Imgproc.line(buf, projectedPoints[i], projectedPoints[i+4], blue, thickness);
+        }
+
+        // Base lines
+        //Imgproc.line(buf, projectedPoints[0], projectedPoints[1], blue, thickness);
+        //Imgproc.line(buf, projectedPoints[1], projectedPoints[2], blue, thickness);
+        //Imgproc.line(buf, projectedPoints[2], projectedPoints[3], blue, thickness);
+        //Imgproc.line(buf, projectedPoints[3], projectedPoints[0], blue, thickness);
+
+        // Top lines
+        Imgproc.line(buf, projectedPoints[4], projectedPoints[5], green, thickness);
+        Imgproc.line(buf, projectedPoints[5], projectedPoints[6], green, thickness);
+        Imgproc.line(buf, projectedPoints[6], projectedPoints[7], green, thickness);
+        Imgproc.line(buf, projectedPoints[4], projectedPoints[7], green, thickness);
     }
 
 }
